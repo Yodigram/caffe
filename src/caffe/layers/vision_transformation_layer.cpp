7d4aa6a3e5902e0d257e3709f9f9f608afe5075e
Nikolas Markou <nikolasmarkou@gmail.com>
2017-01-16 16:32:58 +0200

    [caffe] Added extra options to vision transformation layer

--- src/caffe/layers/vision_transformation_layer.cpp
+++ src/caffe/layers/vision_transformation_layer.cpp
@@ -13,6 +13,38 @@
 namespace caffe
 {
 	//==================================================================
+	/* Example prototxt
+	 *
+		layer {
+		  name: "data_vision"
+		  type: "VisionTransformation"
+		  bottom: "data"
+		  top: "data"
+		  vision_transformation_param {
+			noise_std : 0.02
+			noise_mean : 0.0
+			noise_std_small : 0.0
+			rotate_min_angle : -15.0
+			rotate_max_angle : 15.0
+			rotate_fill_value : 0.0
+			per_pixel_multiplier_mean : 0.0
+			per_pixel_multiplier_std : 0.0
+			rescale_probability : 0.25
+			constant_multiplier_mean : 1.0
+			constant_multiplier_std : 0.1
+			constant_multiplier_color_mean : 1.0
+			constant_multiplier_color_std : 0.1
+			scale_mean : 1.0
+			scale_std : 0.1
+			value_cap_min : -1
+			value_cap_max : 1
+		  }
+		  include {
+			phase: TRAIN
+		  }
+		}
+	*/
+	//==================================================================
 
 	template<typename Dtype>
 	void VisionTransformationLayer<Dtype>::LayerSetUp(
@@ -34,6 +66,14 @@ namespace caffe
         m_constantMultiplierMean = Dtype(1.0f);
         m_constantMultiplierStd = Dtype(0.0f);
 
+        m_scaleMean = 1.0f;
+        m_scaleStd = 0.0f;
+        m_constantMultiplierColorMean = Dtype(1.0f);
+        m_constantMultiplierColorStd = Dtype(0.0f);
+
+        m_valueCapMin = Dtype(0.0f);
+        m_valueCapMax = Dtype(0.0f);
+
         if (this->layer_param_.has_vision_transformation_param() == true)
         {
         	VisionTransformationParameter params = this->layer_param_.vision_transformation_param();
@@ -48,6 +88,14 @@ namespace caffe
     		m_rescaleProbability = params.rescale_probability();
     		m_constantMultiplierMean = params.constant_multiplier_mean();
     		m_constantMultiplierStd = params.constant_multiplier_std();
+
+    		m_scaleMean = params.scale_mean();
+    		m_scaleStd = params.scale_std();
+    		m_constantMultiplierColorMean = params.constant_multiplier_color_mean();
+    		m_constantMultiplierColorStd = params.constant_multiplier_color_std();
+
+    		m_valueCapMin = params.value_cap_min();
+    		m_valueCapMax = params.value_cap_max();
         }
 
         DCHECK(m_noiseStd >= 0);
@@ -104,7 +152,7 @@ namespace caffe
 				rescales.data());
 
 		//--------- multiplier distribution
-		std::vector<Dtype> multipliers(number, Dtype(0));
+		std::vector<Dtype> multipliers(number, Dtype(1.0));
 
 		if (m_constantMultiplierMean != 0.0f &&
 			m_constantMultiplierStd != 0.0f)
@@ -116,15 +164,47 @@ namespace caffe
 					multipliers.data());
 		}
 
+		//--------- color multiplier distribution
+		std::vector<Dtype> multipliersColor(number * channels, Dtype(1.0));
+
+		if (m_constantMultiplierColorMean != 0.0f &&
+			m_constantMultiplierColorStd != 0.0f)
+		{
+			caffe_rng_gaussian<Dtype>(
+					number * channels,
+					m_constantMultiplierColorMean,
+					m_constantMultiplierColorStd,
+					multipliersColor.data());
+		}
+
+		//--------- scale distribution
+		std::vector<float> scales(number, 1.0f);
+
+		if (m_scaleMean != 0.0f &&
+			m_scaleStd != 0.0f)
+		{
+			caffe_rng_gaussian<float>(
+					number,
+					m_scaleMean,
+					m_scaleStd,
+					scales.data());
+		}
+
 		//--------- iterate over images in blob
 		#pragma omp parallel for
 		for (int n = 0; n < number; n++)
 		{
 			float angle = angles[n];
+			float scale = scales[n];
 			bool rescale = rescales[n] > 0;
 			Dtype multiplier = std::max(Dtype(0.0), multipliers[n]);
+
 			cv::Mat noiseSlice, noiseSmallSlice, perPixelMultiplierSlice;
-			cv::Mat rotationMatrix = cv::getRotationMatrix2D(center, angle, 1.0);
+			cv::Mat rotationMatrix =
+					cv::getRotationMatrix2D(
+							center,
+							double(angle),
+							double(scale));
 			cv::Mat tmpSlice(height, width, cv::DataType<Dtype>::type);
 
 			//----- add gaussian noise high
@@ -164,11 +244,13 @@ namespace caffe
 			//----- iterate over channels
 			for (int c = 0; c < channels; c++)
 			{
+				Dtype multiplierColor = multipliersColor[n * channels + c];
+				Dtype multiplierTotal = multiplier * multiplierColor;
 				int offset = bottomBlob->offset(n, c, 0, 0);
 				cv::Mat bottomSlice(height, width, cv::DataType<Dtype>::type, bottomData[offset]);
 
 				//----- rotate
-				if (angle == 0.0f)
+				if (angle == 0.0f && scale == 0.0f)
 				{
 					bottomSlice.copyTo(tmpSlice);
 				}
@@ -217,14 +299,32 @@ namespace caffe
 			    }
 
 			    //----- multiply channel
-			    if (multiplier != 0.0f &&
-			    	multiplier != 1.0f)
+			    if (multiplierTotal != Dtype(0.0) &&
+			    	multiplierTotal != Dtype(1.0))
 			    {
-			    	tmpSlice *= multiplier;
+			    	tmpSlice *= multiplierTotal;
+			    }
+
+			    //----- cap everything between min and max
+			    if (m_valueCapMin != m_valueCapMax)
+			    {
+			    	for (cv::MatIterator_<Dtype> iter = tmpSlice.begin<Dtype>();
+			    			iter != tmpSlice.end<Dtype>();
+			    			++iter)
+			    	{
+			    		Dtype tmp = *iter;
+			    		tmp = std::max(m_valueCapMin, std::min(tmp, m_valueCapMax));
+			    		*iter = tmp;
+			    	}
 			    }
 
 			    //---- copy to top blob
-			    tmpSlice.copyTo(cv::Mat(height, width, cv::DataType<Dtype>::type, topData[offset]));
+			    tmpSlice.copyTo(
+			    		cv::Mat(
+			    			height,
+			    			width,
+			    			cv::DataType<Dtype>::type,
+							topData[offset]));
 			}
 		}
 	}
